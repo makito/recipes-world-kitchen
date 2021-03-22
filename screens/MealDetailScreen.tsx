edd69bc6e37b83aaa16eb788e3c39c599c04a81d
Ivan Orekhov <makito82@gmail.com>
2021-03-22 19:35:17 +0300

    basic screens structure

---
+++ screens/MealDetailScreen.tsx
@@ -0,0 +1,18 @@
+import React, { FC } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+export const MealDetailScreen: FC = () => {
+  return (
+    <View style={styles.screen}>
+      <Text>The Meal Detail Screen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+});
